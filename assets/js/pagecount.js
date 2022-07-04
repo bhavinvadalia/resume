e2b62990a0ab7121de6f19e63d2e72dc1af1dca0
unknown <bhavinv89@gmail.com>
2022-07-04 10:35:03 +0000

    Updated view count 3

--- assets/js/pagecount.js
+++ assets/js/pagecount.js
@@ -4,7 +4,7 @@ invokeCount()
 function invokeCount(){
     fetch('https://myapione.azure-api.net/myfirstvisitcounter/HttpTrigger1?name=getcount')
         .then(res =>{
-                res.text.then(r => console.log(r))
+                res.text().then(r => console.log(r))
         })
         
 }
