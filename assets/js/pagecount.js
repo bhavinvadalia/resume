25d8c378922c2e793152fd978026e5d111fb8ebd
unknown <bhavinv89@gmail.com>
2022-07-09 08:50:26 +0000

    added IIFE

--- assets/js/pagecount.js
+++ assets/js/pagecount.js
@@ -1,9 +1,8 @@
-var count = 0;
-invokeCount()
 
-function invokeCount(){
+
+(function invokeCount(){
     fetch('https://myapione.azure-api.net/myfirstvisitcounter/HttpTrigger1?name=getcount')
         .then(response => response.text())
         .then(data => console.log(data));
         
-}
+})();
