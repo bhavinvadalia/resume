fb25a8a98dd6725fa490435f1a7aacde7646d7b4
unknown <bhavinv89@gmail.com>
2022-07-09 09:57:38 +0000

    Updated pagecount

--- assets/js/pagecount.js
+++ assets/js/pagecount.js
@@ -3,6 +3,11 @@
 (function invokeCount(){
     fetch('https://myapione.azure-api.net/myfirstvisitcounter/HttpTrigger1?name=getcount')
         .then(response => response.text())
-        .then(data => console.log(data));
-        
+        .then(data => {
+            myval = JSON.parse(data)
+            console.log(myval.count)
+            document.getElementById('pagec').innerHTML = myval.count
+        });
+    fetch('https://myapione.azure-api.net/myfirstvisitcounter/put-item?name=setcount')
+        .then(response => response.text())
 })();
